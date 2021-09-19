7a34201bd4d0b8250269ff3a6a10817be09cbdbe
Juane <monesvol@gmail.com>
2021-09-19 23:38:37 +0200

    Creació arxius modul 3

---
+++ M3/src/n1/M3_N1F3.java
@@ -0,0 +1,26 @@
+/*
+ * IT ACADEMY -- Itinerari mentoritzat JAVA
+ * 
+ * 		MODUL 3: Nivell 1, Fase 1
+ * 
+ * 		Alumne: Juan Emilio Fernández Reinaldos
+ * 
+ */
+
+package n1;
+
+public class M3_N1F3 {
+
+	public static void main(String[] args) {
+		
+		char[] nom = {'J','u','a','n',' ','E','m','i','l','i','o'};
+		
+		for(int i = 0; i < nom.length; i++) {
+			
+			System.out.println(nom[i]);
+			
+		}
+		
+	}
+
+}
